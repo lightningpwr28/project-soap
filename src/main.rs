6123829eb595807126877df84f089341b45a0287
lightningpwr28 <lightningpwr28@gmail.com>
2024-06-09 18:40:17 -0700

    Switched to a more OOP program structure to make passing around locations better; started on temp dir

--- src/main.rs
+++ src/main.rs
@@ -12,223 +12,249 @@ use vosk::{Model, Recognizer};
 
 // For loading list of swear words
 use std::fs::File;
-use std::io::{self, BufRead, Read};
+use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    let file_location = "test\\Extra Crispy - Crispy reacts to Daily Dose of Internet.webm";
-    let model_location = "vosk\\model\\vosk-model-en-us-0.22-lgraph";
+    let file_location =
+        String::from("Extra Crispy - Crispy reacts to Daily Dose of Internet.webm");
+    let model_location = String::from("vosk/model/vosk-model-en-us-0.22-lgraph");
 
     let start = Instant::now();
 
-    find_and_remove_curses(
-        file_location,
-        &preprocess_audio(file_location),
-        model_location,
-    );
+    let cleaner = Cleaner::new(model_location, file_location);
+    cleaner.preprocess_audio();
+    cleaner.find_and_remove_curses();
 
     let end = Instant::now();
 
     println!("Filtering took {:#?}", end.duration_since(start));
 }
 
-fn preprocess_audio(file_location: &str) -> String {
-    let preprocessed_file_location = format!("{}.wav", file_location);
-
-    let out = Command::new("ffmpeg")
-        .arg("-y")
-        .args(["-i", &format!("{}", file_location)])
-        .args(["-ar", "16000"])
-        .args(["-ac", "1"])
-        //.args(["-f", "s16le"])
-        .arg(&preprocessed_file_location)
-        .output()
-        .expect("FFmpeg error");
-    println!("{:?}", out);
-    return preprocessed_file_location;
+struct Cleaner {
+    model_location: String,
+    file_location: String,
+    preprocessed_file_location: String,
 }
-
-fn find_and_remove_curses(file_location: &str, preprocessed_file_location: &str, model_path: &str) {
-    // Load the Vosk model
-    let model = Model::new(model_path).expect("Could not create model");
-
-    // Open the WAV file
-    let mut reader =
-        hound::WavReader::open(preprocessed_file_location).expect("Could not open WAV file");
-
-    // Check if audio file is mono PCM
-    if reader.spec().channels != 1 || reader.spec().sample_format != hound::SampleFormat::Int {
-        panic!("Audio file must be WAV format mono PCM.");
-    }
-
-    // Get the samples from the WAV file
-    let samples = reader
-        .samples()
-        .collect::<hound::Result<Vec<i16>>>()
-        .expect("Could not read WAV file");
-
-    let thread_number = thread::available_parallelism().expect("Error getting system available parallelism").into();
-    let mut sample_chunks = samples.chunks_exact(samples.len() / thread_number);
-
-    let mut threads: Vec<JoinHandle<()>> = Vec::new();
-    
-
-    for i in 0..thread_number {
-        let mut recognizer =
-            Recognizer::new(&model, 16000 as f32).expect("Could not create recognizer");
-        recognizer.set_words(true);
-
-        let samples_half = sample_chunks.next().unwrap().to_vec();
-
-        let thread_file_location = file_location.clone();
-
-        let thread = thread::spawn(move || {
-            split_threads(&mut recognizer, samples_half, &format!("{:?}", i))
-        });
-
-        threads.push(thread);
-    }
-
-    for thread in threads {
-        thread.join().expect("Error joining threads");
-    }
-
-    let mut times_in: Vec<vosk::Word> = Vec::new();
-    let mut file_contents: Vec<String> = vec![String::new(); thread_number];
-
-    let mut counter = 0;
-    let offset: f32 = (samples.len() as f32) / (thread_number as f32);
-    for i in file_contents.iter_mut() {
-        *i = fs::read_to_string(format!("temp\\remove_at_{:?}_{}.json", counter, file_location)).expect(&format!("Error opening json file at remove_at_{:?}_{}.json", counter, file_location));
-        let mut json: Vec<vosk::Word> =
-            serde_json::from_str(i).expect("Error in deserializing json");
-        
-        for word in json.iter_mut() {
-            word.start += (offset / 16000.) * counter as f32;
-            word.end += (offset / 16000.) * counter as f32;
+impl Cleaner {
+    fn new(model_location: String, file_location: String) -> Cleaner {
+        Cleaner::make_temp_dir();
+        Cleaner {
+            model_location,
+            file_location: file_location.clone(),
+            preprocessed_file_location: format!("temp/{}.wav", file_location.clone()),
         }
-        
-        times_in.append(&mut json);
-        counter += 1;
     }
 
-    let curse_list = load_expletives();
+    fn preprocess_audio(&self) {
+        let out = Command::new("ffmpeg")
+            .arg("-y")
+            .args(["-i", &format!("{}", self.file_location)])
+            .args(["-ar", "16000"])
+            .args(["-ac", "1"])
+            //.args(["-f", "s16le"])
+            .arg(self.preprocessed_file_location.clone())
+            .output()
+            .expect("FFmpeg error");
+        println!("{:?}", out);
+    }
 
-    let clean_file_location = remove_curses(times_in.as_slice(), file_location, curse_list);
-    clean_up(file_location, &clean_file_location, thread_number);
-}
+    fn find_and_remove_curses(&self) {
+        // Load the Vosk model
+        let model = Model::new(self.model_location.clone()).expect("Could not create model");
 
-// Calls the FFmpeg command line program to remove the audio of the expletives from the video or audio file the user puts in
-// times_in is an array of locations where expletives are in the file at file_location
-fn remove_curses(times_in: &[vosk::Word], file_location: &str, curses: HashSet<String>) -> String {
-    // Stores the list of filters that determine which audio segments will be cut out
-    let mut filter_string = String::new();
-    let mut number_of_curses = 0;
+        // Open the WAV file
+        let mut reader = hound::WavReader::open(self.preprocessed_file_location.clone())
+            .expect("Could not open WAV file");
 
-    // This loops over each expletive in times_in and converts the data into a filter FFmpeg can use.
-    for curse in times_in {
-        if curses.contains(curse.word) {
-            filter_string.push_str(&format!(
-                "volume=enable='between(t,{},{})':volume=0, ",
-                curse.start, curse.end
+        // Check if audio file is mono PCM
+        if reader.spec().channels != 1 || reader.spec().sample_format != hound::SampleFormat::Int {
+            panic!("Audio file must be WAV format mono PCM.");
+        }
+
+        // Get the samples from the WAV file
+        let samples = reader
+            .samples()
+            .collect::<hound::Result<Vec<i16>>>()
+            .expect("Could not read WAV file");
+
+        let thread_number = thread::available_parallelism()
+            .expect("Error getting system available parallelism")
+            .into();
+        let mut sample_chunks = samples.chunks_exact(samples.len() / thread_number);
+
+        let mut threads: Vec<JoinHandle<()>> = Vec::new();
+
+        for i in 0..thread_number {
+            let mut recognizer =
+                Recognizer::new(&model, 16000 as f32).expect("Could not create recognizer");
+            recognizer.set_words(true);
+
+            let samples_half = sample_chunks.next().unwrap().to_vec();
+
+            let loc = self.file_location.clone();
+
+            let thread = thread::spawn(move || {
+                Cleaner::split_threads(loc, &mut recognizer, samples_half, &format!("{:?}", i))
+            });
+
+            threads.push(thread);
+        }
+
+        for thread in threads {
+            thread.join().expect("Error joining threads");
+        }
+
+        let mut times_in: Vec<vosk::Word> = Vec::new();
+        let mut file_contents: Vec<String> = vec![String::new(); thread_number];
+
+        let mut counter = 0;
+        let offset: f32 = (samples.len() as f32) / (thread_number as f32);
+        for i in file_contents.iter_mut() {
+            *i = fs::read_to_string(format!("temp/{:?}_'{}'.json", counter, self.file_location))
+            .expect(&format!(
+                "Error opening json file at {:?}_{}.json",
+                counter, self.file_location
             ));
+            let mut json: Vec<vosk::Word> =
+                serde_json::from_str(i).expect("Error in deserializing json");
 
-            println!("Removed {} at {} to {}", curse.word, curse.start, curse.end);
+            for word in json.iter_mut() {
+                word.start += (offset / 16000.) * counter as f32;
+                word.end += (offset / 16000.) * counter as f32;
+            }
 
-            number_of_curses += 1;
+            times_in.append(&mut json);
+            counter += 1;
+        }
+
+        let curse_list = Cleaner::load_expletives();
+
+        let clean_file_location = self.remove_curses(times_in.as_slice(), curse_list);
+        self.clean_up(&clean_file_location);
+    }
+
+    fn remove_curses(&self, times_in: &[vosk::Word], curses: HashSet<String>) -> String {
+        // Stores the list of filters that determine which audio segments will be cut out
+        let mut filter_string = String::new();
+        let mut number_of_curses = 0;
+
+        // This loops over each expletive in times_in and converts the data into a filter FFmpeg can use.
+        for curse in times_in {
+            if curses.contains(curse.word) {
+                filter_string.push_str(&format!(
+                    "volume=enable='between(t,{},{})':volume=0, ",
+                    curse.start, curse.end
+                ));
+
+                println!("Removed {} at {} to {}", curse.word, curse.start, curse.end);
+
+                number_of_curses += 1;
+            }
+        }
+
+        // If left unedited, the last two characters would be ', ', which we don't want.
+        filter_string.pop();
+        filter_string.pop();
+
+        println!("{}", filter_string);
+
+        let mut file_location_string = self.file_location.to_string();
+        file_location_string.insert_str(
+            file_location_string
+                .find('.')
+                .expect("Couldn't find file type"),
+            "-clean",
+        );
+
+        // This builds the command.
+        let out = Command::new("ffmpeg")
+            .arg("-y")
+            .arg("-i")
+            .arg(self.file_location.clone())
+            .arg("-af")
+            .arg(filter_string)
+            .args(["-c:v", "copy"])
+            .arg(&format!("{}", file_location_string))
+            .output()
+            .expect("failed to execute process");
+
+        println!("{:?}", out);
+
+        println!("Removed {} expletives.", number_of_curses);
+        return file_location_string;
+    }
+
+    fn load_expletives() -> HashSet<String> {
+        let mut list = HashSet::<String>::new();
+
+        if let Ok(lines) = read_lines("list.txt") {
+            // Consumes the iterator, returns an (Optional) String
+            for line in lines.flatten() {
+                if !line.starts_with("/") && line != "" {
+                    list.insert(line);
+                }
+            }
+        }
+
+        return list;
+
+        fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+        where
+            P: AsRef<Path>,
+        {
+            let file = File::open(filename)?;
+            Ok(io::BufReader::new(file).lines())
         }
     }
 
-    // If left unedited, the last two characters would be ', ', which we don't want.
-    filter_string.pop();
-    filter_string.pop();
+    fn split_threads(
+        file_location: String,
+        recognizer: &mut Recognizer,
+        samples: Vec<i16>,
+        thread_name: &str,
+    ) {
+        // Feed the model the sound file. I do this all at once because I don't care about real-time output.
+        recognizer.accept_waveform(&samples);
 
-    println!("{}", filter_string);
+        let binding = recognizer
+            .final_result()
+            .single()
+            .expect("Error in outputting result");
+        let curses = binding.result;
 
-    let mut file_location_string = file_location.to_string();
-    file_location_string.insert_str(
-        file_location_string
-            .find('.')
-            .expect("Couldn't find file type"),
-        "-clean",
-    );
+        let name = format!("temp/{}_'{}'.json", thread_name, file_location);
+        println!("{}", name);
 
-    // This builds the command.
-    let out = Command::new("ffmpeg")
-        .arg("-y")
-        .arg("-i")
-        .arg(file_location)
-        .arg("-af")
-        .arg(filter_string)
-        .args(["-c:v", "copy"])
-        .arg(&format!("{}", file_location_string))
-        .output()
-        .expect("failed to execute process");
+        fs::write(name, json!(curses).to_string()).expect(&format!("Error outputting thread {} json to file", thread_name));
 
-    println!("{:?}", out);
+        println!("Thread {} done!", thread_name);
+    }
 
-    println!("Removed {} expletives.", number_of_curses);
-    return file_location_string;
-}
+    fn clean_up(&self, clean_file_location: &str) {
+        let clean_file =
+            fs::read(clean_file_location).expect("Error reading clean file for clean up");
+        fs::write(self.file_location.clone(), clean_file)
+            .expect("Error copying clean file to original");
 
-fn load_expletives() -> HashSet<String> {
-    let mut list = HashSet::<String>::new();
+        let paths = fs::read_dir("./temp").unwrap();
+        for file in paths {
+            let path_str: String =
+                String::from(file.unwrap().path().file_name().unwrap().to_str().unwrap());
 
-    if let Ok(lines) = read_lines("list.txt") {
-        // Consumes the iterator, returns an (Optional) String
-        for line in lines.flatten() {
-            if !line.starts_with("/") && line != "" {
-                list.insert(line);
+            if path_str.contains(&self.file_location) {
+                fs::remove_file(path_str.clone())
+                    .expect(&format!("Unable to remove file at {}", path_str));
             }
         }
     }
 
-    return list;
-
-    fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-    where
-        P: AsRef<Path>,
-    {
-        let file = File::open(filename)?;
-        Ok(io::BufReader::new(file).lines())
+    fn make_temp_dir() {
+        let here = fs::canonicalize("./").expect("Error in canonicalizing temp path");
+        let temp_dir_location =  here.display().to_string() + "\\temp";
+        if !std::path::Path::new(&temp_dir_location).exists() {
+            fs::create_dir(temp_dir_location).expect("Error in creating temp dir");
+        }
+        
     }
-}
-
-fn split_threads(recognizer: &mut Recognizer, samples: Vec<i16>, thread_name: &str) {
-    // Feed the model the sound file. I do this all at once because I don't care about real-time output.
-    recognizer.accept_waveform(&samples);
-
-    let binding = recognizer
-        .final_result()
-        .single()
-        .expect("Error in outputting result");
-    let curses = binding.result;
-
-    fs::write(
-        format!("temp\\remove_at_{}.json", thread_name),
-        json!(curses).to_string(),
-    )
-    .expect(&format!(
-        "Error outputting thread {} json to file",
-        thread_name
-    ));
-
-    println!("Thread {} done!", thread_name);
-}
-
-fn clean_up(file_location: &str, clean_file_location: &str, thread_number: usize) {
-    
-    let clean_file = fs::read(clean_file_location).expect("Error reading clean file for clean up");
-    fs::write(file_location, clean_file).expect("Error copying clean file to original");
-
-    for i in 0..thread_number {
-        let path = format!("temp\\remove_at_{:?}_{}.json", i, file_location);
-        fs::remove_file(path.clone()).expect(&format!("Unable to remove file at {}", path));
-    }
-
-
-
-
-
-
 }
