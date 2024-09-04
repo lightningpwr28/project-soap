0ce27a4f55c1d1fa0133f70dfa538261d9708f8f
lightningpwr28 <lightningpwr28@gmail.com>
2024-09-03 18:04:21 -0700

    Implemented deserialization of whisperx json output

--- src/backends/whisperx_local.rs
+++ src/backends/whisperx_local.rs
@@ -1,5 +1,10 @@
+use dirs::home_dir;
+
+use serde::Deserialize;
+use serde_json::from_str;
+
 use crate::{backends::Cleaner, cli};
-use std::process::Command;
+use std::{fs::File, io::{self, Read}, process::Command};
 
 pub struct WhisperXLocal {
     file_location: String,
@@ -9,6 +14,60 @@ impl WhisperXLocal {
     fn setup() {
         // here I need to install the dependencies of WhisperX and WhisperX itself
         todo!()
+    }
+
+    fn serialize(&self) -> Vec<super::Word>{
+
+        #[derive(Deserialize)]
+        struct WhisperXJson {
+            segments: Vec<WhisperXSegment>,
+            language: String
+        }
+
+        #[derive(Deserialize)]
+        struct WhisperXSegment {
+            start: f32,
+            end: f32,
+            text: String,
+            words: Vec<WhisperXWord>
+
+        }
+
+        #[derive(Deserialize)]
+        struct WhisperXWord {
+            word: String,
+            start: f32,
+            end: f32,
+            score: f32
+        }
+
+        impl From<WhisperXWord> for crate::backends::Word {
+            fn from(value: WhisperXWord) -> Self {
+                let word = value.word.to_string();
+                let start = value.start;
+                let end = value.end;
+
+                crate::backends::Word { word, start, end }
+            }
+        }
+
+        // here I need to serialize the output of whisperx
+        // Using the json output, I think the structure is like {segments: {text, words [what we actually want]}}
+        let mut file = File::open(self.file_location.clone()).expect("Error opening transcription file");
+        let mut json_string = String::new();
+        file.read_to_string(&mut json_string).expect("Error serializing json");
+
+        let json: WhisperXJson = from_str(&json_string).expect("Error getting Value form json");
+
+        let mut words: Vec<crate::backends::Word> = Vec::new();
+
+        for segment in json.segments {
+            for word in segment.words {
+                words.push(word.into());
+            }
+        }
+
+        return words;
     }
 
     pub fn from_args(args: cli::Args) -> Option<Box<dyn Cleaner>> {
@@ -43,13 +102,31 @@ impl Cleaner for WhisperXLocal {
     fn transcribe(&mut self) -> Vec<super::Word> {
         let out = Command::new("whisperx")
             .arg(self.file_location.clone())
-            .args(["--highlight_words", "True"])
+            .args([
+                "--output_dir",
+                &{if cfg!(windows) {
+                    String::from(
+                        home_dir()
+                            .expect("Error getting user's home directory")
+                            .to_str()
+                            .expect("Error converting user's home directory to string"),
+                    ) + &String::from("\\.project-soap\\temp")
+                } else {
+                    String::from(
+                        home_dir()
+                            .expect("Error getting user's home directory")
+                            .to_str()
+                            .expect("Error converting user's home directory to string"),
+                    ) + &String::from("/.project-soap/temp")
+                }},
+            ])
+            .arg("--highlight_words True")
             .arg("--output_format json")
             .arg(self.other_options.clone())
             .output()
             .expect("Error running WhisperX");
 
         print!("{:#?}", out);
-        return Vec::new();
+        return self.serialize();
     }
 }
